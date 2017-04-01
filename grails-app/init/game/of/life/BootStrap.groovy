58d37eb751bc1a1d2353e026b04c1ad2374e0d12
Qun Chen <qun.chen@jaxsta.com>
2017-04-01 16:49:09 +1100

    initialise block pattern

--- grails-app/init/game/of/life/BootStrap.groovy
+++ grails-app/init/game/of/life/BootStrap.groovy
@@ -3,7 +3,23 @@ package game.of.life
 class BootStrap {
 
     def init = { servletContext ->
+        initBlockPattern()
+
     }
     def destroy = {
+    }
+
+    private initBlockPattern(){
+        Pattern block=new Pattern(name: "block").save()
+        Generation generationInitial=new Generation(step: 0)
+
+        generationInitial.addToCells(new Cell(col: 1,row: 1,isAlive: true))
+        generationInitial.addToCells(new Cell(col: 2,row: 1,isAlive: true))
+        generationInitial.addToCells(new Cell(col: 1,row: 2,isAlive: true))
+        generationInitial.addToCells(new Cell(col: 2,row: 2,isAlive: true))
+
+        generationInitial.save()
+
+        block.addToGenerations(generationInitial).save()
     }
 }
