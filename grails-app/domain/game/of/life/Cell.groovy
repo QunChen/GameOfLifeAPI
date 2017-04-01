e3fc12e465f7467a5a8cea161ce495d2f4064657
Qun Chen <qun.chen@jaxsta.com>
2017-04-01 16:48:27 +1100

    change to integer add generation

--- grails-app/domain/game/of/life/Cell.groovy
+++ grails-app/domain/game/of/life/Cell.groovy
@@ -2,56 +2,14 @@ package game.of.life
 
 class Cell {
 
-    short col
-    short row
-    boolean isAlive
+    Long id
+    Integer col
+    Integer row
+    Boolean isAlive
+    Generation generation
 
     static mapping = {
-        id genetor: 'assigned'
         version false
-    }
-
-    static namedQueries = {
-
-        topNeighbour{
-            eq 'col', col
-            eq 'row', row-1
-         }
-
-        bottomNeighbour{
-            eq 'col', col
-            eq 'row', row+1
-        }
-
-        leftNeighbour{
-            eq 'col', col-1
-            eq 'row', row
-        }
-
-        rightNeighbour{
-            eq 'col', col+1
-            eq 'row', row
-        }
-
-        topRightNeighbour{
-            eq 'col', col+1
-            eq 'row', row-1
-        }
-
-        bottomRightNeighbour{
-            eq 'col', col+1
-            eq 'row', row+1
-        }
-
-        topLeftNeighbour{
-            eq 'col', col-1
-            eq 'row', row-1
-        }
-
-        bottomLeftNeighbour{
-            eq 'col', col-1
-            eq 'row', row+1
-        }
     }
 
     static constraints = {
